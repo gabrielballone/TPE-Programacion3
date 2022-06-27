20443563ed4e0170ff8be22710bd0d9563aebca1
German De Francesco <germandefrancesco@hotmail.com>
2022-06-27 19:08:16 -0300

    separated business domain from CSVReader

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 public class App {
+
+    static final String cvsSplitBy = ",";
 
     public static void main(String[] args) throws Exception {
         stage1();
@@ -9,9 +12,34 @@ public class App {
     public static void stage1() {
         final String inputPath = "tpe1\\dataset1.csv";
         final String outputPath = "tpe1\\result1.csv";
-        LinkedList<Genre> genresList = CSVReader.readCSV(inputPath);
+        LinkedList<String> genresLines = CSVReader.readCSV(inputPath);
+        LinkedList<Genre> genreList = new LinkedList<Genre>();
+        for (String line : genresLines) {
+            String[] items = line.split(cvsSplitBy);
+            String title = items[0];
+            String author = items[1];
+            int pagesCount = Integer.parseInt(items[2]);
+            String genresString = items[3];
+            String[] genres = genresString.split(" ");
+            Book book = new Book(title, author, pagesCount, Arrays.asList(genres));
+            boolean found = false;
+            for (String genre : genres) {
+                for (Genre genreL : genreList) {
+                    if (genreL.equals(genre)) {
+                        genreL.addBook(book);
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    Genre genreToAdd = new Genre(genre);
+                    genreToAdd.addBook(book);
+                    genreList.add(genreToAdd);
+                }
+                found = false;
+            }
+        }
         Genre selectedGenre = null;
-        for (Genre genre : genresList) {
+        for (Genre genre : genreList) {
             if(genre.getName().equals("thriller")){
                 selectedGenre = genre;
                 break;
@@ -25,5 +53,7 @@ public class App {
     public static void stage2() {
         final String inputPath = "tpe2\\dataset1.csv";
         final String outputPath = "tpe2\\result1.csv";
+        DirectedGraph<Integer> graph = new DirectedGraph<Integer>();
+
     }
 }
--- src/CSVReader.java
+++ src/CSVReader.java
@@ -2,16 +2,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class CSVReader {
 
-    public static LinkedList<Genre> readCSV(String filePathRelative) {
+    public static LinkedList<String> readCSV(String filePathRelative) {
         String filePathAbsolute = new File("").getAbsolutePath();
         String line = "";
-        String cvsSplitBy = ",";
-        LinkedList<Genre> genreList = new LinkedList<Genre>();
+        LinkedList<String> lines = new LinkedList<String>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePathAbsolute + "\\" + filePathRelative))) {
             boolean isFirstLine = true;
             while ((line = br.readLine()) != null) {
@@ -19,33 +17,11 @@ public class CSVReader {
                     isFirstLine = false;
                     continue;
                 }
-                String[] items = line.split(cvsSplitBy);
-                String title = items[0];
-                String author = items[1];
-                int pagesCount = Integer.parseInt(items[2]);
-                String genresString = items[3];
-                String[] genres = genresString.split(" ");
-                Book book = new Book(title, author, pagesCount, Arrays.asList(genres));
-                boolean found = false;
-                for (String genre : genres) {
-                    for (Genre genreL : genreList) {
-                        if (genreL.equals(genre)) {
-                            genreL.addBook(book);
-                            found = true;
-                        }
-                    }
-                    if (!found) {
-                        Genre genreToAdd = new Genre(genre);
-                        genreToAdd.addBook(book);
-                        genreList.add(genreToAdd);
-                    }
-                    found = false;
-                }
+                lines.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return genreList;
+        return lines;
     }
-    
 }
