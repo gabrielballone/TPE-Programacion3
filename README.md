dba661ddd5b75259cabf59fa37f9b84ce20cb89e
German De Francesco <germandefrancesco@hotmail.com>
2022-06-27 17:59:56 -0300

    separeted tpe1 datasets from tpe2 in different folders, moved and renamed pdfs inside each one

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TPE-Programacion3
+
+Copy datasets for statement 1 in tpe1 folder, and do the same for 2 in tpe2 folder.
--- src/App.java
+++ src/App.java
@@ -1,15 +1,14 @@
 import java.util.LinkedList;
 public class App {
 
-    static final String inputPath = "dataset1.csv";
-    static final String outputPath = "result1.csv";
-
     public static void main(String[] args) throws Exception {
         stage1();
         //stage2();
     }
 
     public static void stage1() {
+        final String inputPath = "tpe1\\dataset1.csv";
+        final String outputPath = "tpe1\\result1.csv";
         LinkedList<Genre> genresList = CSVReader.readCSV(inputPath);
         Genre selectedGenre = null;
         for (Genre genre : genresList) {
@@ -24,6 +23,7 @@ public class App {
     }
 
     public static void stage2() {
-
+        final String inputPath = "tpe2\\dataset1.csv";
+        final String outputPath = "tpe2\\result1.csv";
     }
 }
